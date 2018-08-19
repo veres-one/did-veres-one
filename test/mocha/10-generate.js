b07682477f5c1221c39ced7fc1fe8a06c90012da
Manu Sporny <msporny@digitalbazaar.com>
2018-08-19 15:49:06 -0400

    Update tests to support Ed25519 cryptonyms.

--- test/mocha/10-generate.js
+++ test/mocha/10-generate.js
@@ -9,8 +9,9 @@ const expect = global.chai.expect;
 describe('Veres One generate DIDs', () => {
   const didv1 = require('../../lib');
 
-  it('should generate protected nym-based DID Document', async () => {
+  it('should generate protected RSA nym-based DID Document', async () => {
     const nymOptions = {
+      keyType: 'RsaVerificationKey2018',
       passphrase: 'foobar'
     };
     const didDocument = await didv1.generate(nymOptions);
@@ -26,8 +27,9 @@ describe('Veres One generate DIDs', () => {
       .to.have.string('-----BEGIN ENCRYPTED PRIVATE KEY-----');
   }).timeout(30000);
 
-  it('should generate unprotected nym-based DID Document', async () => {
+  it('should generate unprotected RSA nym-based DID Document', async () => {
     const nymOptions = {
+      keyType: 'RsaVerificationKey2018',
       passphrase: null
     };
     const didDocument = await didv1.generate(nymOptions);
@@ -41,6 +43,41 @@ describe('Veres One generate DIDs', () => {
       didDocument.privateDidDocument.authentication[0].publicKey[0]
         .privateKey.privateKeyPem)
       .to.have.string('-----BEGIN RSA PRIVATE KEY-----');
+  }).timeout(30000);
+
+  it('should generate protected ed25519 nym-based DID Document', async () => {
+    const nymOptions = {
+      keyType: 'Ed25519VerificationKey2018',
+      passphrase: 'foobar'
+    };
+    const didDocument = await didv1.generate(nymOptions);
+
+    expect(didDocument.publicDidDocument.id).to.match(
+      /^did\:v1\:test\:nym\:z.*/);
+    expect(didDocument.privateDidDocument.authentication[0].publicKey[0].id)
+      .to.have.string('nym:z');
+    expect(didDocument.privateDidDocument.authentication[0].publicKey[0]
+      .privateKey.jwe.ciphertext)
+      .to.have.lengthOf.above(128);
+    expect(didDocument.publicDidDocument.authentication[0].publicKey[0]
+      .publicKeyBase58)
+      .to.have.lengthOf(44);
+  }).timeout(30000);
+
+  it('should generate unprotected ed25519 nym-based DID Document', async () => {
+    const nymOptions = {
+      keyType: 'Ed25519VerificationKey2018',
+      passphrase: null
+    };
+    const didDocument = await didv1.generate(nymOptions);
+
+    expect(didDocument.publicDidDocument.id).to.match(
+      /^did\:v1\:test\:nym\:z.*/);
+    expect(didDocument.privateDidDocument.authentication[0].publicKey[0].id)
+      .to.have.string('nym:z');
+    expect(didDocument.publicDidDocument.authentication[0].publicKey[0]
+      .publicKeyBase58)
+      .to.have.lengthOf(44);
   }).timeout(30000);
 
   it('should generate uuid-based DID Document', async () => {
