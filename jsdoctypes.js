2e69d942a4e6ef0c8795183d54152c781e151041
Andrew L Jones <ajones@digitalbazaar.com>
2019-03-10 18:35:04 +0000

    Add jsdoc linter to project.
    Use eslint to fix 58 jsdoc lint issues.
    
    Reduce lint errors by about 100.
    
    Reduce jsdoc errors to 102.
    
    Setup eslint with jsdoc.
    
    Correct jsdoc types.
    
    Remove extra space after hyphen.
    
    Updated docs.

---
+++ jsdoctypes.js
@@ -0,0 +1,11 @@
+/**
+ * Veres One has 3 modes dev, test, and live
+ * @typedef {'live'|'test'|'dev'} Mode
+ */
+
+/**
+ * DID type can be a nym (a Cryptonym-based Decentralized Identifier) or
+ * a [uuid](https://en.wikipedia.org/wiki/Universally_unique_identifier)
+ * @typedef {'nym' | 'uuid' } DIDType
+ */
+
