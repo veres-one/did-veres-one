b22ae1b41810960cbfc42a4529b4cb10c5cd4a57
andrewjones <ajones@digitalbazaar.com>
2021-06-10 13:51:34 -0400

    Remove console.error for test suite.

--- tests/VeresOne.spec.js
+++ tests/VeresOne.spec.js
@@ -332,7 +332,6 @@ describe('methods/veres-one', () => {
       try {
         result = await v1.register({didDocument});
       } catch(e) {
-        console.error(e);
         error = e;
       }
       expect(error).not.to.exist;
